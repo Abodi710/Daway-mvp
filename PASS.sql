4c06677a31afa363af8f8e03a6dcbaaac1f9efe8
motman911 <motman911@users.noreply.github.com>
2026-09-19 21:28:56 +0200

    حذف الهياكل القديمة وملفات السجلات
    - حذف مجلد daway-mvp القديم
    - حذف مجلد daway-prod وتحريك محتوياته للجذر
    - حذف ملفات السجلات والملفات المؤقتة
    - حذف ملفات JS القديمة من الجذر
    - حذف ملفات i18n والتحقق القديمة

--- PASS.sql
+++
@@ -1,3 +0,0 @@
-UPDATE users 
-SET password_hash = '$2b$10$rejoPdTLCppmOlqZog8tPen0bspqAkcpyq4ersfZnpv5gkT9DIW32' 
-WHERE email = 'admin@daway.com';
--- daway-prod/src/formatCurrency.js
+++
@@ -1,8 +0,0 @@
-export const formatCurrency = (amount, currency = 'ريال') => {
-  const num = Number(amount);
-  if (isNaN(num)) return '0.00 ' + currency;
-  return num.toLocaleString('ar-SA', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }) + ' ' + currency;
-};
